434c8e70170468b0c50cb446199f72475d43f181
teaocat <teaocat@gmail.com>
2018-01-10 23:35:28 +0800

    Complete Functions.playground
    Complete Functions.playground

--- StudySwift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ StudySwift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -3,3 +3,358 @@
 import Foundation
 
 /// Functions
+
+/// Defining and Calling Functions 函数的定义与调用
+func greet(person: String) -> String {
+    let greeting = "hello, " + person + "!"
+    return greeting
+}
+
+print(greet(person: "Anna"))
+// 打印 "Hello, Anna!"
+print(greet(person: "Brian"))
+// 打印 "Hello, Brian!"
+print("以上在\(#line)打印\n----------")
+
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+print(greetAgain(person: "Anna"))
+// 打印 "Hello again, Anna!"
+print("以上在\(#line)打印\n----------")
+
+
+/// Function Parameters and Return Values 函数参数与返回值
+
+/// Functions Without Parameters 无参数函数
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+// 打印 "hello, world"
+print("以上在\(#line)打印\n----------")
+
+/// Functions With Multiple Parameters 多参数函数
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+// 打印 "Hello again, Tim!"
+print("以上在\(#line)打印\n----------")
+
+/// Functions Without Return Values 无返回值函数
+func greet2(person: String) {
+    print("Hello, \(person)!")
+}
+greet2(person: "Dave")
+// 打印 "Hello, Dave!"
+print("以上在\(#line)打印\n----------")
+/*
+ 注意
+ 严格上来说，虽然没有返回值被定义，greet(person:) 函数依然返回了值。没有定义返回类型的函数会返回一个特殊的Void值。它其实是一个空的元组（tuple），没有任何元素，可以写成()。
+ */
+
+/// 被调用时，一个函数的返回值可以被忽略：
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+func printWithoutCounting(string: String) {
+    let _ = printAndCount(string: string)
+}
+printAndCount(string: "hello, world")
+// 打印 "hello, world" 并且返回值 12
+printWithoutCounting(string: "hello, world")
+// 打印 "hello, world" 但是没有返回任何值
+print("以上在\(#line)打印\n----------")
+/*
+ 注意:
+ 返回值可以被忽略，但定义了有返回值的函数必须返回一个值，如果在函数定义底部没有返回任何值，将导致编译时错误（compile-time error）。
+ */
+
+/// Functions with Multiple Return Values 多重返回值函数
+// 你可以用元组（tuple）类型让多个值作为一个复合值从函数中返回。
+// 下例中定义了一个名为 minMax(array:) 的函数，作用是在一个 Int 类型的数组中找出最小值与最大值。
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+/*
+ minMax(array:) 函数返回一个包含两个 Int 值的元组，这些值被标记为 min 和 max ，以便查询函数的返回值时可以通过名字访问它们。
+ 
+ 在 minMax(array:) 的函数体中，在开始的时候设置两个工作变量 currentMin 和 currentMax 的值为数组中的第一个数。然后函数会遍历数组中剩余的值并检查该值是否比 currentMin 和 currentMax 更小或更大。最后数组中的最小值与最大值作为一个包含两个 Int 值的元组返回。
+ 
+ 因为元组的成员值已被命名，因此可以通过 . 语法来检索找到的最小值与最大值：
+ */
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+// 打印 "min is -6 and max is 109"
+print("以上在\(#line)打印\n----------")
+
+/// Optional Tuple Return Types 可选元组返回类型
+/*
+ 如果函数返回的元组类型有可能整个元组都“没有值”，你可以使用可选的（ optional ） 元组返回类型反映整个元组可以是nil的事实。你可以通过在元组类型的右括号后放置一个问号来定义一个可选元组，例如 (Int, Int)? 或 (String, Int, Bool)?
+ 注意 可选元组类型如 (Int, Int)? 与元组包含可选类型如 (Int?, Int?) 是不同的.可选的元组类型，整个元组是可选的，而不只是元组中的每个元素值。
+ 
+ 前面的 minMax(array:) 函数返回了一个包含两个 Int 值的元组。但是函数不会对传入的数组执行任何安全检查，如果 array 参数是一个空数组，如上定义的 minMax(array:) 在试图访问 array[0] 时会触发一个运行时错误(runtime error)。
+ 
+ 为了安全地处理这个“空数组”问题，将 minMax(array:) 函数改写为使用可选元组返回类型，并且当数组为空时返回 nil：
+ */
+func minMax2(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return nil }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+// 你可以使用可选绑定来检查 minMax(array:) 函数返回的是一个存在的元组值还是 nil：
+if let bounds = minMax2(array: [8, -6, 2, 109, 3, 71]) {
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+// 打印 "min is -6 and max is 109"
+print("以上在\(#line)打印\n----------")
+
+/// Function Argument Labels and Parameter Names 函数参数标签和参数名称
+/*
+ 每个函数参数都有一个参数标签( argument label )以及一个参数名称( parameter name )。参数标签在调用函数的时候使用；调用的时候需要将函数的参数标签写在对应的参数前面。参数名称在函数的实现中使用。默认情况下，函数参数使用参数名称来作为它们的参数标签。
+ */
+func someFunction(firstParameterName: Int, secondParameterName: Int) {
+    // 在函数体内，firstParameterName 和 secondParameterName 代表参数中的第一个和第二个参数值
+}
+someFunction(firstParameterName: 1, secondParameterName: 2)
+// 所有的参数都必须有一个独一无二的名字。虽然多个参数拥有同样的参数标签是可能的，但是一个唯一的函数标签能够使你的代码更具可读性。
+
+/// Specifying Argument Labels 指定参数标签
+// 你可以在参数名称前指定它的参数标签，中间以空格分隔：
+func someFunction(argumentLabel parameterName: Int) {
+    // 在函数体内，parameterName 代表参数值
+}
+// 这个版本的 greet(person:) 函数，接收一个人的名字和他的家乡，并且返回一句问候：
+func greet(person: String, from hometown: String) -> String {
+    return "Hello \(person)!  Glad you could visit from \(hometown)."
+}
+print(greet(person: "Bill", from: "Cupertino"))
+// 打印 "Hello Bill!  Glad you could visit from Cupertino."
+print("以上在\(#line)打印\n----------")
+
+/// Omitting Argument Labels 忽略参数标签
+// 如果你不希望为某个参数添加一个标签，可以使用一个下划线(_)来代替一个明确的参数标签。
+func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
+    // 在函数体内，firstParameterName 和 secondParameterName 代表参数中的第一个和第二个参数值
+}
+someFunction(1, secondParameterName: 2)
+
+/// Default Parameter Values 默认参数值
+// 你可以在函数体中通过给参数赋值来为任意一个参数定义默认值（Deafult Value）。当默认值被定义后，调用这个函数时可以忽略这个参数。
+func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+    // 如果你在调用时候不传第二个参数，parameterWithDefault 会值为 12 传入到函数体中。
+}
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6) // parameterWithDefault = 6
+someFunction(parameterWithoutDefault: 4) // parameterWithDefault = 12
+print("以上在\(#line)打印\n----------")
+
+/// Variadic Parameters 可变参数
+/*
+ 一个可变参数（variadic parameter）可以接受零个或多个值。函数调用时，你可以用可变参数来指定函数参数可以被传入不确定数量的输入值。通过在变量类型名后面加入（...）的方式来定义可变参数。
+ 
+ 可变参数的传入值在函数体中变为此类型的一个数组。例如，一个叫做 numbers 的 Double... 型可变参数，在函数体内可以当做一个叫 numbers 的 [Double] 型的数组常量。
+ 
+ 下面的这个函数用来计算一组任意长度数字的 算术平均数（arithmetic mean)：
+ */
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+arithmeticMean(1, 2, 3, 4, 5)
+// 返回 3.0, 是这 5 个数的平均数。
+arithmeticMean(3, 8.25, 18.75)
+// 返回 10.0, 是这 3 个数的平均数。
+print("以上在\(#line)打印\n----------")
+/*
+ 注意：
+ 一个函数最多只能拥有一个可变参数。
+ */
+
+/// In-Out Parameters 输入输出参数
+/*
+ 函数参数默认是常量。试图在函数体中更改参数值将会导致编译错误(compile-time error)。这意味着你不能错误地更改参数值。如果你想要一个函数可以修改参数的值，并且想要在这些修改在函数调用结束后仍然存在，那么就应该把这个参数定义为输入输出参数（In-Out Parameters）。
+ 
+ 定义一个输入输出参数时，在参数定义前加 inout 关键字。一个输入输出参数有传入函数的值，这个值被函数修改，然后被传出函数，替换原来的值。想获取更多的关于输入输出参数的细节和相关的编译器优化，请查看输入输出参数一节。
+ 
+ 你只能传递变量给输入输出参数。你不能传入常量或者字面量，因为这些量是不能被修改的。当传入的参数作为输入输出参数时，需要在参数名前加 & 符，表示这个值可以被函数修改。
+ 
+ 注意 输入输出参数不能有默认值，而且可变参数不能用 inout 标记。
+ */
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+// swapTwoInts(_:_:) 函数简单地交换 a 与 b 的值。该函数先将 a 的值存到一个临时常量 temporaryA 中，然后将 b 的值赋给 a，最后将 temporaryA 赋值给 b。
+print("以上在\(#line)打印\n----------")
+
+// 你可以用两个 Int 型的变量来调用 swapTwoInts(_:_:)。需要注意的是，someInt 和 anotherInt 在传入 swapTwoInts(_:_:) 函数前，都加了 & 的前缀：
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+// 打印 "someInt is now 107, and anotherInt is now 3"
+print("以上在\(#line)打印\n----------")
+/*
+ 注意：
+ 输入输出参数和返回值是不一样的。上面的 swapTwoInts 函数并没有定义任何返回值，但仍然修改了 someInt 和 anotherInt 的值。输入输出参数是函数对函数体外产生影响的另一种方式。
+ */
+
+/// Function Types 函数类型
+// 每个函数都有种特定的函数类型，函数的类型由函数的参数类型和返回类型组成。
+// 例如：
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+/*
+ 这个例子中定义了两个简单的数学函数：addTwoInts 和 multiplyTwoInts。这两个函数都接受两个 Int 值， 返回一个 Int 值。
+ 
+ 这两个函数的类型是 (Int, Int) -> Int，可以解读为“这个函数类型有两个 Int 型的参数并返回一个 Int 型的值。”。
+ */
+// 下面是另一个例子，一个没有参数，也没有返回值的函数：
+func printHelloWorld() {
+    print("hello, world")
+}
+// 这个函数的类型是：() -> Void，或者叫“没有参数，并返回 Void 类型的函数”。
+
+/// Using Function Types 使用函数类型
+// 在 Swift 中，使用函数类型就像使用其他类型一样。例如，你可以定义一个类型为函数的常量或变量，并将适当的函数赋值给它：
+var mathFunction: (Int, Int) -> Int = addTwoInts
+/*
+ 这段代码可以被解读为：
+ 
+ ”定义一个叫做 mathFunction 的变量，类型是‘一个有两个 Int 型的参数并返回一个 Int 型的值的函数’，并让这个新变量指向 addTwoInts 函数”。
+ 
+ addTwoInts 和 mathFunction 有同样的类型，所以这个赋值过程在 Swift 类型检查(type-check)中是允许的。
+ 
+ 现在，你可以用 mathFunction 来调用被赋值的函数了：
+ */
+print("Result: \(mathFunction(2, 3))")
+// Prints "Result: 5"
+print("以上在\(#line)打印\n----------")
+
+// 有相同匹配类型的不同函数可以被赋值给同一个变量，就像非函数类型的变量一样：
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+// Prints "Result: 6"
+print("以上在\(#line)打印\n----------")
+
+// 就像其他类型一样，当赋值一个函数给常量或变量时，你可以让 Swift 来推断其函数类型：
+let anotherMathFunction = addTwoInts
+// anotherMathFunction 被推断为 (Int, Int) -> Int 类型
+
+
+/// Function Types as Parameter Types 函数类型作为参数类型
+/*
+ 你可以用 (Int, Int) -> Int 这样的函数类型作为另一个函数的参数类型。这样你可以将函数的一部分实现留给函数的调用者来提供。
+ 下面是另一个例子，正如上面的函数一样，同样是输出某种数学运算结果：
+ */
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+// 打印 "Result: 8"
+print("以上在\(#line)打印\n----------")
+/*
+ 这个例子定义了 printMathResult(_:_:_:) 函数，它有三个参数：第一个参数叫 mathFunction，类型是 (Int, Int) -> Int，你可以传入任何这种类型的函数；第二个和第三个参数叫 a 和 b，它们的类型都是 Int，这两个值作为已给出的函数的输入值。
+ 
+ 当 printMathResult(_:_:_:) 被调用时，它被传入 addTwoInts 函数和整数 3 和 5。它用传入 3 和 5 调用 addTwoInts，并输出结果：8。
+ 
+ printMathResult(_:_:_:) 函数的作用就是输出另一个适当类型的数学函数的调用结果。它不关心传入函数是如何实现的，只关心传入的函数是不是一个正确的类型。这使得 printMathResult(_:_:_:) 能以一种类型安全（type-safe）的方式将一部分功能转给调用者实现。
+ */
+
+
+/// Function Types as Return Types 函数类型作为返回类型
+/*
+ 你可以用函数类型作为另一个函数的返回类型。你需要做的是在返回箭头（->）后写一个完整的函数类型。
+ 
+ 下面的这个例子中定义了两个简单函数，分别是 stepForward(_:) 和 stepBackward(_:)。stepForward(_:)函数返回一个比输入值大 1 的值。stepBackward(_:) 函数返回一个比输入值小 1 的值。这两个函数的类型都是 (Int) -> Int：
+ */
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+// 如下名为 chooseStepFunction(backward:) 的函数，它的返回类型是 (Int) -> Int 类型的函数。chooseStepFunction(backward:) 根据布尔值 backwards 来返回 stepForward(_:) 函数或 stepBackward(_:) 函数：
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+// 你现在可以用 chooseStepFunction(backward:) 来获得两个函数其中的一个：
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero 现在指向 stepBackward() 函数。
+/*
+ 上面这个例子中计算出从 currentValue 逐渐接近到0是需要向正数走还是向负数走。currentValue 的初始值是 3，这意味着 currentValue > 0 为真（true），这将使得 chooseStepFunction(_:) 返回 stepBackward(_:) 函数。一个指向返回的函数的引用保存在了 moveNearerToZero 常量中。
+ */
+// 现在，moveNearerToZero指向了正确的函数，它可以被用来数到零：
+print("Counting to zero:")
+// Counting to zero:
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+// 3...
+// 2...
+// 1...
+// zero!
+print("以上在\(#line)打印\n----------")
+
+
+/// Nested Functions 嵌套函数
+/*
+ 到目前为止本章中你所见到的所有函数都叫全局函数（global functions），它们定义在全局域中。你也可以把函数定义在别的函数体中，称作 嵌套函数（nested functions）。
+ 
+ 默认情况下，嵌套函数是对外界不可见的，但是可以被它们的外围函数（enclosing function）调用。一个外围函数也可以返回它的某一个嵌套函数，使得这个函数可以在其他域中被使用。
+ 
+ 你可以用返回嵌套函数的方式重写 chooseStepFunction(backward:) 函数：
+ */
+func chooseStepFunction2(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+var currentValue2 = -4
+let moveNearerToZero2 = chooseStepFunction2(backward: currentValue > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue2 != 0 {
+    print("\(currentValue2)... ")
+    currentValue2 = moveNearerToZero2(currentValue2)
+}
+print("zero!")
+// -4...
+// -3...
+// -2...
+// -1...
+// zero!
+print("以上在\(#line)打印\n----------")
